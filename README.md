cb8417f555ee4634c56b617e09efe98190e127e3
Marc Breitung <info@marcbreitung.de>
2019-07-09 18:44:37 +0200

    Added clap

--- README.md
+++ README.md
@@ -2,3 +2,23 @@
 
 ## WIP
 Generate TYPO3 custom content elements via command line.
+
+### Generate a new extension
+
+With long arguments
+
+```bash
+custom-content extension --key <key> --title <title> --description <description>
+```
+
+With short arguments
+
+```bash
+custom-content extension -k <key> -t <title> -d <description>
+```
+
+Example
+
+```bash
+custom-content extension -k new_extension -t Title -d Description
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+
 use crate::element::element::Element;
 use crate::extension::conf::Conf;
 use crate::extension::extension::Extension;
@@ -10,20 +15,101 @@ mod file;
 mod files;
 
 fn main() {
-    let element_text = Element::new(Key::new("content_elements", "text"))
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .subcommand(SubCommand::with_name("extension")
+            .about("Add a new extension")
+            .arg(Arg::with_name("key")
+                .short("k")
+                .long("key")
+                .help("Sets the extension key")
+                .required(true)
+                .takes_value(true)
+            )
+            .arg(Arg::with_name("title")
+                .short("t")
+                .long("title")
+                .help("Sets the extension title")
+                .required(true)
+                .takes_value(true)
+            )
+            .arg(Arg::with_name("description")
+                .short("d")
+                .long("description")
+                .help("Sets the extension description")
+                .required(true)
+                .takes_value(true)
+            )
+        )
+        .subcommand(SubCommand::with_name("element")
+            .about("Add a new element")
+            .arg(Arg::with_name("key")
+                .short("k")
+                .long("key")
+                .help("Sets the element key")
+                .required(true)
+                .takes_value(true)
+            )
+            .arg(Arg::with_name("title")
+                .short("t")
+                .long("title")
+                .help("Sets the element title")
+                .required(true)
+                .takes_value(true)
+            )
+            .arg(Arg::with_name("description")
+                .short("d")
+                .long("description")
+                .help("Sets the element description")
+                .required(true)
+                .takes_value(true)
+            )
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("extension") {
+        if matches.is_present("key") {
+            let key = matches.value_of("key").unwrap();
+            let title = matches.value_of("title").unwrap();
+            let description = matches.value_of("description").unwrap();
+            let conf = Conf::new()
+                .key(&key)
+                .title(&title)
+                .description(&description);
+            let extension = Extension::new(conf).build();
+            println!("Added new extension {:?} ", key);
+        }
+    }
+
+    if let Some(matches) = matches.subcommand_matches("element") {
+        if matches.is_present("key") {
+            let key = matches.value_of("key").unwrap();
+            let title = matches.value_of("title").unwrap();
+            let description = matches.value_of("description").unwrap();
+            let conf = Conf::new()
+                .key(&key)
+                .title(&title)
+                .description(&description);
+            let extension = Extension::new(conf).build();
+            println!("Added new element {:?} ", key);
+        }
+    }
+
+    /*
+
+    let element_text = Element::new(Key::new(&extension_key, "text"))
         .icon("text");
 
-    let element_image = Element::new(Key::new("content_elements", "image"))
+    let element_image = Element::new(Key::new(&extension_key, "image"))
         .icon("image");
-
-    let conf = Conf::new()
-        .key("content_elements")
-        .title("Content Elements")
-        .description("Content Elements Extension");
 
     let extension = Extension::new(conf)
         .add_element(element_text)
         .add_element(element_image);
 
     extension.build();
+
+    */
 }
